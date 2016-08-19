a7061406b618d0e63677f9c84b9b4de3fd255190
PerezContrerasLuis <LuisPerezContreras@gmail.com>
2016-08-19 10:58:51 -0500

    suma de dos numero

---
+++ ejemplosBasicos/suma.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Suma</title>
+</head>
+<body>
+
+	<?php
+		$numero = 1;
+		$numeroDos = 2;
+
+		$suma =  $numero + $numeroDos;
+
+		echo "La suma de ".$numero." + ".$numeroDos." = ".$suma;
+	?>
+</body>
+</html>
