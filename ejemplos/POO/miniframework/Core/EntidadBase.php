9281c08e5031b1ae2c8785c62b6febc7afe8e25b
PerezContrerasLuis <luisperezcontreras@gmail.com>
2025-08-02 13:56:30 -0600

    docs moc EntidadBase

--- ejemplos/POO/miniframework/Core/EntidadBase.php
+++ ejemplos/POO/miniframework/Core/EntidadBase.php
@@ -20,14 +20,20 @@ abstract class EntidadBase
     }
 
     public function save(array $data)
-    {
+    {   
+         //$data = ["id"=>"1","nombre"=>"luis","email"=>"luis@gmail.com"]
         if (isset($data['id']) && $data['id'] > 0) {
+            // UPDATE table SET id=?,nombre=?,email=? WHERE id = ?
+            //fields = "id=?,nombre=?,email=?"
             $fields = implode(' = ?, ', array_keys($data)) . ' = ?';
             $sql = "UPDATE {$this->table} SET {$fields} WHERE id = ?";
             $stmt = $this->db->prepare($sql);
             $values = array_values($data);
             $values[] = $data['id'];
         } else {
+            // INSERT INTO table id,nombre,email VALUES (?,?,?)
+            //fields = id,nombre, email 
+            // placeholder = ?,?,?
             $fields = implode(', ', array_keys($data));
             $placeholders = implode(', ', array_fill(0, count($data), '?'));
             $sql = "INSERT INTO {$this->table} ({$fields}) VALUES ({$placeholders})";
